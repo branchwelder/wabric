051267f34781a4a95898666ad9599d34f34acd72
branchwelder <htwiggsmith@gmail.com>
2023-07-18 09:33:05 -0700

    add svg download

--- index.js
+++ index.js
@@ -110,6 +110,33 @@ pane
   })
   .on("change", runSimulation);
 
+pane
+  .addButton({
+    title: "Download SVG",
+  })
+  .on("click", () => {
+    const serializer = new XMLSerializer();
+    const svg = document.querySelector("svg").cloneNode(true);
+
+    svg.setAttributeNS(
+      "http://www.w3.org/2000/xmlns/",
+      "xmlns:xlink",
+      "http://www.w3.org/1999/xlink"
+    );
+
+    const source = serializer.serializeToString(svg);
+    const svgURL =
+      "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
+
+    const downloadLink = document.createElement("a");
+    downloadLink.href = svgURL;
+    downloadLink.download = "wabric.svg";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+    document.body.removeChild(downloadLink);
+  });
+
 /////////////////////////////////////////////////////////////////
 // Forces
 /////////////////////////////////////////////////////////////////
@@ -475,6 +502,22 @@ function drawFaces() {
           .on("drag", (e) => dragged(e, "face"))
           .on("end", (e) => dragEnd(e, "face"))
       );
+  } else {
+    d3.selectAll(".faces").selectChildren().remove();
+  }
+}
+
+function drawShading() {
+  if (paused) return;
+
+  if (PARAMS.showShading) {
+    // Create polygons for all faces
+    face = faceContainer
+      .selectAll()
+      .data(faces)
+      .join("group")
+      .selectAll()
+      .join("line");
   } else {
     d3.selectAll(".faces").selectChildren().remove();
   }
